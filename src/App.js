433ee99d8f9578e85017b49189c7579ca2847eb3
yeowonNa <mhyh716@daum.net>
2021-08-08 17:04:13 +0900

    총점, 기록 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
+import { useState } from 'react';
 import Button from './button';
 import Dice from './Dice';
 
-function App() {
-    return(
-        <div>
-            <div>
-                <Button text="던지기"/>
-                <Button text="처음부터"/>
-            </div>
-            <Dice color="red" num={2} />
-        </div>
-    )
+function random(n) {
+    return Math.ceil(Math.random() * n);
 }
 
-export default App;
+function App() {
+    const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [gameHistory, setGameHistory] = useState([]);
+
+    const handleRollClick = () => {
+        const nextNum = random(6);
+        setNum(nextNum);
+        setSum(sum + nextNum);
+        //참조형(배열, 객체 등) state를 변경할 때에는 전체를 새로 만든다고 생각
+        setGameHistory([...gameHistory, nextNum]); 
+    }
+
+    const handleClearClick = () => {
+        setNum(1);
+        setSum(0);
+        setGameHistory([]);
+    }
+    return (
+      <div>
+        <div>
+          <Button onClick={handleRollClick}>던지기</Button>
+          <Button onClick={handleClearClick}>처음부터</Button>
+        </div>
+        <div>
+            <h2>나</h2>
+            <Dice color="red" num={num} />
+            <h2>총점</h2>
+            <p>{sum}</p>
+            <h2>기록</h2>
+            <p>{gameHistory.join(', ')}</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
