3c698893fede985175485de8caf334eef6754e4e
yeowonNa <mhyh716@daum.net>
2021-08-09 19:43:03 +0900

    add to css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import { useState } from 'react';
 import Board from "./Board";
-import Button from './button';
+import Button from './Button';
+import './App.css';
+import logo from './assets/logo.png'
 
 function random(n) {
-    return Math.ceil(Math.random() * n);
+	return Math.ceil(Math.random() * n);
 }
 
 function App() {
-    const [myHistory, setMyHistory] = useState([]);
-    const [otherHistory, setOtherHistory] = useState([]);
+	const [myHistory, setMyHistory] = useState([]);
+	const [otherHistory, setOtherHistory] = useState([]);
 
     const handleRollClick = () => {
-        const nextMyNum = random(6);
-        const nextOtherNum = random(6);
-        
-        setMyHistory([...myHistory, nextMyNum]);
-        setOtherHistory([...otherHistory, nextOtherNum]);
+			const nextMyNum = random(6);
+			const nextOtherNum = random(6);
+			
+			setMyHistory([...myHistory, nextMyNum]);
+			setOtherHistory([...otherHistory, nextOtherNum]);
     }
 
     const handleClearClick = () => {
-        setMyHistory([]);
-        setOtherHistory([]);
+			setMyHistory([]);
+			setOtherHistory([]);
     }
 
     return (
-        <div>
-            <div>
-                <Button onClick={handleRollClick}>던지기</Button>
-                <Button onClick={handleClearClick}>처음부터</Button>
-            </div>
-            <div>
-                <Board name="나" color="blue" gameHistory={myHistory}/>
-                <Board name="상대" color="red" gameHistory={otherHistory}/>
-            </div>
-        </div>
+			<div className="App">
+				<div>
+					<img className="App-logo" src={logo} alt="주사위게임 로고" />
+					<h1 className="App-title">주사위게임</h1>
+					<div>
+						<Button className="App-button" color="blue" onClick={handleRollClick}>던지기</Button>
+						<Button className="App-button" color="red" onClick={handleClearClick}>처음부터</Button>
+					</div>
+				</div>
+					<div className="Board-heading">
+						<Board 
+							className="App-board"
+							name="나" 
+							color="blue" 
+							gameHistory={myHistory}/>
+						<Board name="상대" color="red" gameHistory={otherHistory}/>
+					</div>
+			</div>
     )
 }
 
